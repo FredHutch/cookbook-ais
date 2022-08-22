1fe14e8734226ad2b8d0e8d6f4ebd9c99f1fc7d7
Michael Gutteridge <michael.gutteridge@gmail.com>
2022-08-22 16:47:11 -0700

    Reorder to install autofs package first

--- recipes/ldap_server.rb
+++ recipes/ldap_server.rb
@@ -5,8 +5,8 @@
 # Copyright:: 2022, The Authors, All Rights Reserved.
 #
 # install and configure openldap server to serve automount info
-include_recipe 'openldap::default'
-
 package 'autofs-ldap' do
   action :install
 end
+
+include_recipe 'openldap::default'
