56dec9739df20f4739fb6f527ebc8c84b3e77667
Michael Gutteridge <michael.gutteridge@gmail.com>
2022-08-22 15:32:25 -0700

    Add more attributes for OpenLDAP cookbook, including new schema list

--- attributes/ldaps.rb
+++ attributes/ldaps.rb
@@ -1,6 +1,17 @@
 # This is a password for TESTING ONLY.  It's "letmein"
 node.default['openldap']['rootpw'] = \
-  '{SSHA}b7a875fc1ea228b9061041b7cec4bd3c52ab3ce3'
+  '{SSHA}BKUwewI6N9Yat00Aoxn/nNUEq78ifdif'
 node.default['openldap']['server'] = 'ldap.example.org'
 node.default['openldap']['basedn'] = 'dc=example,dc=org'
+node.default['openldap']['cn'] = 'admin'
+node.default['openldap']['admin_cn'] = 'administrators'
 
+# Required
+#
+node.default['openldap']['schemas'] = %w(
+  core.schema
+  cosine.schema
+  inetorgperson.schema
+  nis.schema
+  autofs.schema
+)
