b61174d1520fd5d13d895ac0795c1af83b805374
Michael Gutteridge <michael.gutteridge@gmail.com>
2022-08-26 09:14:33 -0700

    Add syncrepl attributes

--- attributes/ldaps.rb
+++ attributes/ldaps.rb
@@ -1,17 +1,23 @@
+
+# This is a password for TESTING ONLY.  It's "letmein"
+node.default['openldap']['rootpw'] = \
+  '{SSHA}BKUwewI6N9Yat00Aoxn/nNUEq78ifdif'
+node.default['openldap']['slapd_replpw'] = 'onetwothree'
+node.default['openldap']['syncrepl_cn'] = 'cn=syncrole'
+
+# Default attributes for testing
+# #FIXME: move these into kitchen
+node.default['openldap']['server'] = '127.0.0.1'
+node.default['openldap']['basedn'] = 'dc=example,dc=org'
+node.default['openldap']['cn'] = 'admin'
+node.default['openldap']['admin_cn'] = 'administrators'
+
 node.default['ais']['master_map_name'] = '/etc/auto.master'
 # use this form if using auto.master from LDAP instead of /etc:
 # node.default['ais']['master_map_name'] = \
 #   'ou=auto.master,ou=automount,ou=ais,dc=example,dc=org'
 node.default['ais']['ldap_uri'] = 'ldap://127.0.0.1'
 node.default['ais']['search_base'] = 'ou=automount,ou=ais,dc=example,dc=org'
-
-# This is a password for TESTING ONLY.  It's "letmein"
-node.default['openldap']['rootpw'] = \
-  '{SSHA}BKUwewI6N9Yat00Aoxn/nNUEq78ifdif'
-node.default['openldap']['server'] = '127.0.0.1'
-node.default['openldap']['basedn'] = 'dc=example,dc=org'
-node.default['openldap']['cn'] = 'admin'
-node.default['openldap']['admin_cn'] = 'administrators'
 
 # Required- do not change/override without knowing the impact
 #
@@ -22,3 +28,4 @@ node.default['openldap']['schemas'] = %w(
   nis.schema
   autofs.schema
 )
+node.default['openldap']['slapd_provider'] = 'localhost'
