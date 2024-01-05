a4e9152fccc9793cc85c3f285d1ded9fb6f8a3c0
Michael Gutteridge <michael.gutteridge@gmail.com>
2024-01-05 11:00:15 -0800

    Add slapd template override for ldap_consumer

--- recipes/ldap_consumer.rb
+++ recipes/ldap_consumer.rb
@@ -16,3 +16,12 @@ node.override['openldap']['slapd_rid'] = \
   node['ais']['consumers'].find_index(node['hostname'])
 
 include_recipe 'openldap::default'
+
+# Override openldap cookbook template for slapd.conf
+#  - our version removes limits for syncrepl bind
+#  - also removes sorting of schemas
+begin
+  r = resources(template: "#{openldap_dir}/slapd.conf")
+  r.cookbook 'ais'
+  r.source 'openldap/slapd.conf.erb'
+end
